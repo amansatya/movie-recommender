5632a287e35b21d1a2451f248d8cfc5dc3696e10
SATYA AMAN <satyaaman1012@gmail.com>
2025-10-03 01:58:42 +0530

    Setup React project with Vite and Tailwind v4, cleaned boilerplate files

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl font-bold">Movie Recommendation App 🎬</h1>
+        </div>
+    );
+}
+
+export default App;
